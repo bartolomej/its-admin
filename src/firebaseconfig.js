aab354ac391b3beafe39d73a714d5bb2acaae122
bartolomej <bartolomej.kozorog@gmail.com>
2019-11-22 21:53:29 +0100

    Added firebase config.

---
+++ src/firebaseconfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyBfy8jU0c9dRCUvf0eJNjvM3MXbvhZ1f1c",
+  authDomain: "its-rd.firebaseapp.com",
+  databaseURL: "https://its-rd.firebaseio.com",
+  projectId: "its-rd",
+  storageBucket: "its-rd.appspot.com",
+  messagingSenderId: "51459712366",
+  appId: "1:51459712366:web:bd330d9858002052424d7f",
+  measurementId: "G-607XDPBE4R"
+};
